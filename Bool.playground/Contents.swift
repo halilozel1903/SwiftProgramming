6a230e65fa94b51f4087fce30248992f514ba7ef
Halil Özel <halilozel1903@gmail.com>
2018-10-07 16:03:42 +0300

    first commit

---
+++ Bool.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+// Bool :  true or false return values.
+
+// if - else used in buildings.
+
+var isPaid : Bool = false // default value : false
+
+isPaid = true // change value : true
+
+if isPaid{ // true value
+    
+    print("Payment completed")
+}else{ // false value
+    print("Payment not completed")
+}
