e3c539f42e5a010de57b5b07f7df7521a89766cf
Halil Özel <halilozel1903@gmail.com>
2018-10-07 17:32:31 +0300

    add codes

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -10,3 +10,9 @@ import UIKit
 var myTuples = (58,"Halil") // tuples (int,string)
 print(myTuples.0) // int value
 print(myTuples.1) // String value
+
+
+let test = (404,"Error") // tuples
+let (statusCode,status) = test // new variable
+print(statusCode) // 404
+
