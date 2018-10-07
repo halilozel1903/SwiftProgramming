d0cfefc0987fbcc0f36bf2090a9481e8b90e85f4
Halil Özel <halilozel1903@gmail.com>
2018-10-07 16:43:39 +0300

    Tuples example add

---
+++ Tuples.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+/*
+ 
+ Tuples group multiple values into a single compound value.
+ The values within a tuple can be of any type and don’t have to be of the same type as each other.
+ 
+ */
+
+var myTuples = (58,"Halil") // tuples (int,string)
+print(myTuples.0) // int value
+print(myTuples.1) // String value
