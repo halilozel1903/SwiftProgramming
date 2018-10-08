0b392f1c6e2c89853df23199f3227275d46efa25
Halil Özel <halilozel1903@gmail.com>
2018-10-08 17:34:08 +0300

    add  function lesson

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+func sum(number1:Int,number2:Int)->Int{ // int type function
+    
+    return number1 + number2 // sum
+}
+
+print(sum(number1: 12, number2: 21)) // result
+
+
+func showName(){ // basic function
+    
+    print("Swift 4.2 Programming") // print func
+}
+
+showName() // result
